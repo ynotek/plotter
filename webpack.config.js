0307cf526e9d3c0e96723da0f2a713f0edcf9291
Clément Lucas <clement.lucas@swimbot.net>
2016-07-27 17:20:44 +0200

    Init repo
    Signed-off-by: Clément Lucas <clement.lucas@swimbot.net>

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+var BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+
+module.exports = {
+  entry: "./app/components/Main.js",
+  output: {
+    filename: "public/bundle.js"
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel',
+        query: {
+          presets: ['react', 'es2015']
+        }
+      }
+    ]
+  },
+  plugins: [
+    new BrowserSyncPlugin({
+      // browse to http://localhost:3000/ during development,
+      // ./public directory is being served
+      host: 'localhost',
+      port: 3000,
+      server: { baseDir: ['public'] }
+    })
+  ]
+}
